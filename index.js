7381863f903371c042e9b18153ec43afa47ee2bc
ranuarga <august.arga@gmail.com>
2021-06-09 01:51:38 +0700

    Add: Time with MomentJS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const moment = require('moment')
 const mqtt = require('mqtt')
 const options = {
     host: 'broker.hivemq.com',
@@ -14,18 +14,22 @@ client.on('connect', function () {
           Math.random() * (max - min + 1) + min
         )
     }
+    let randPh, randTemperature, randDo, randAmonia, randDutycycle, stringChain
+    const maxAmonia = 2
+    const minAmonia = 0
+    const feedOne = 0
+    const feedTwo = 0
     setInterval(function() {
-        let feedOne = 0
-        let randPh = between(7, 8)
-        let randTemperature = between(22, 30)
-        let randDo = between(8, 12)
-        let randAmonia = between(0, 2)
-        let feedTwo = 0
-        let randDutycycle = between(47, 60)
-        let stringChain = feedOne + '#' + randPh + '#' + randTemperature + '#' + 
+        randPh = between(7, 8)
+        randTemperature = between(22, 30)
+        randDo = between(8, 12)
+        randAmonia = ((Math.random() * (maxAmonia - minAmonia)) + minAmonia).toFixed(2)
+        randDutycycle = between(47, 60)
+        stringChain = feedOne + '#' + randPh + '#' + randTemperature + '#' + 
             randDo + '#' + randAmonia + '#' + feedTwo + '#' + randDutycycle
         client.publish(process.env.DEVICE_ID, stringChain)
-    }, 5000);
+        console.log(stringChain + ' ' + moment().format('YYYY-MM-DD HH:mm:ss.SSS'))
+    }, 5000)
 })
 
 client.on('error', function (error) {

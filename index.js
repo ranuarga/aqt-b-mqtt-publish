803406a309dfa1e1c82746592a16c11233000059
ranuarga <august.arga@gmail.com>
2021-06-26 22:57:49 +0700

    Add: CSV Writer to Write Log

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ require('dotenv').config()
 const moment = require('moment')
 const mqtt = require('mqtt')
 const dataset = require('./dataset')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'log.csv',
+    append: true,
+    header: [
+        {id: 'feedOne', title: 'feedOne'},
+        {id: 'ph', title: 'ph'},
+        {id: 'temperature', title: 'temperature'},
+        {id: 'do', title: 'do'},
+        {id: 'amonia', title: 'amonia'},
+        {id: 'feedTwo', title: 'feedTwo'},
+        {id: 'dutycycle', title: 'dutycycle'},
+        {id: 'time', title: 'time'},
+    ]
+});
 const options = {
     host: 'broker.hivemq.com',
     port: 1883,
 }
 const client = mqtt.connect(options)
-let randPh, randTemperature, randDo, randAmonia, randDutycycle, stringChain, line
+let randPh, randTemperature, randDo, randAmonia, randDutycycle, stringChain, line, time
+let writeData = []
 let interval = null
 const maxAmonia = 2
 const minAmonia = 0
@@ -39,16 +55,33 @@ client.on('message', function (topic, message) {
                     randDo = between(8, 12)
                     randAmonia = ((Math.random() * (maxAmonia - minAmonia)) + minAmonia).toFixed(2)
                     randDutycycle = between(47, 60)
-                    stringChain = feedOne + '#' + randPh + '#' + randTemperature + '#' + 
-                        randDo + '#' + randAmonia + '#' + feedTwo + '#' + randDutycycle
                 } else if (process.env.DATA_TYPE == 'DATASET') {
                     line = between(0,dataset.data.length - 1)
                     dataSensor = dataset.data[line]
-                    stringChain = feedOne + '#' + dataSensor.ph + '#' + dataSensor.temperature + '#' + 
-                        dataSensor.do + '#' + dataSensor.amonia + '#' + feedTwo + '#' + dataSensor.dutycycle
+                    randPh = dataSensor.ph
+                    randTemperature = dataSensor.temperature
+                    randDo = dataSensor.do
+                    randAmonia = dataSensor.amonia
+                    randDutycycle = dataSensor.dutycycle
                 }
+                stringChain = feedOne + '#' + randPh + '#' + randTemperature + '#' + 
+                        randDo + '#' + randAmonia + '#' + feedTwo + '#' + randDutycycle
                 client.publish(process.env.DEVICE_ID, stringChain)
-                console.log(stringChain + ' ' + moment().format('YYYY-MM-DD HH:mm:ss.SSS'))
+                time = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
+                console.log(stringChain + ' ' + time)
+                writeData = [
+                    {
+                        "feedOne": feedOne,
+                        "ph": randPh,
+                        "temperature": randTemperature,
+                        "do": randDo,
+                        "amonia": randAmonia,
+                        "feedTwo": feedTwo,
+                        "dutycycle": randDutycycle,
+                        "time": time
+                    }
+                ]
+                csvWriter.writeRecords(writeData).then(() => console.log('Data Logged'))
             }, 5000)
         } else if (message.toString() == '0') {
             clearInterval(interval)
